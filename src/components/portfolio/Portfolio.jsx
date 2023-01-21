f7877e8f0e19cbc51df285b04cd85084554237fb
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-22 01:55:29 +0800

    Added sensor project site in projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.png'
 import IMG5 from '../../assets/portfolio5.png'
@@ -23,12 +24,12 @@ const Portfolio = () => {
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="TruthSeeker.AI" />
+            <img src={IMG2} alt="Sensor website" />
           </div>
-          <h3>TruthSeeker.AI</h3>
+          <h3>Website for Arduino sensors</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/FranzKhylOng/TruthSeeker.AI" className='btn' target='_blank'>Github</a>
-            <a href="https://docs.google.com/presentation/d/e/2PACX-1vSAUqLn2SX62ZoI3-ozGDOZXonZNic1SBkQHcXVV2oMWII9cC6CkpKn7dSiCFPA6g/pub?start=false&loop=false&delayms=5000" className='btn btn-primary' target='_blank'>Pitch Deck</a>
+            <a href="https://github.com/calso-christian/Sensor-Project-Website" className='btn' target='_blank'>Github</a>
+            <a href="https://www.canva.com/design/DAFYUay7J-I/cnkRjilgxypGcO_RuUKVdg/view?utm_content=DAFYUay7J-I&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" className='btn btn-primary' target='_blank'>Presentation</a>
           </div>
         </article>
         <article className="portfolio__item">
