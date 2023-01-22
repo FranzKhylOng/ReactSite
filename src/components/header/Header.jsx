4134bba93fbad3cbdfae47b6226d0d15a58c6474
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-23 01:42:01 +0800

    Animation for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,31 +3,40 @@ import './header.css'
 import ME from '../../assets/me.png'
 import HeaderSocial from './HeaderSocials'
 import Resume from '../../assets/Resume.pdf'
+import { Fade, Slide } from "react-awesome-reveal";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-
-        <div className="pic">
-          <div className="me">
-            <img src={ME} alt="me" />
+        <Fade>
+          <div className="pic">
+            <div className="me">
+              <img src={ME} alt="me" />
+            </div>
           </div>
-        </div>
+        </Fade>
 
-        <div className="text">
-          <h5 className='introtxt'>Hello! My name is</h5>
+          <div className="text">
+            <Slide direction='down'>
+            <h5 className='introtxt'>Hello! My name is</h5>
             <h1>Franz Khyl Ong</h1>
-          <h5 className="text-light introtxt">A 4th Year Computer Engineering Student</h5>
-          <div className='cta'>
-            <a href={Resume} download className='btn'>Resume</a>
-            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-          </div>
-        </div>
+            </Slide>
+             
+            <Slide direction='up'>
+            <h5 className="text-light introtxt">A 4th Year Computer Engineering Student</h5>
+            <div className='cta'>
+              <a href={Resume} download className='btn'>Resume</a>
+              <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+            </div>
+            </Slide>
 
+          </div>
       </div>
+      <Fade delay={1000}>
       <HeaderSocial />
       <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </Fade>
     </header>
   )
 }
