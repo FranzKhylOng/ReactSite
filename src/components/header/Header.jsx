34591ace17e504f9736e0d8e7234e8b233a6d07d
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2023-01-22 22:28:33 +0800

    New layout in desktop view

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,17 +8,23 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-          <h1>Franz Khyl Ong</h1>
-        <h5 className="text-light">Computer Engineering Student</h5>
-        <div className='cta'>
-          <a href={Resume} download className='btn'>Resume</a>
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        <div className="pic">
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
         </div>
-        
-        <div className="me">
-          <img src={ME} alt="me" />
+
+        <div className="text">
+          <h5 className='introtxt'>Hello! My name is</h5>
+            <h1>Franz Khyl Ong</h1>
+          <h5 className="text-light introtxt">A 4th Year Computer Engineering Student</h5>
+          <div className='cta'>
+            <a href={Resume} download className='btn'>Resume</a>
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          </div>
         </div>
+
       </div>
       <HeaderSocial />
       <a href="#contact" className='scroll__down'>Scroll Down</a>
